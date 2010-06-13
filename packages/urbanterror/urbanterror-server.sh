7087596c86fd6b1bbccf33abb961da36a5b1de64
Jelle van der Waa <jellevdwaa@gmail.com>
2010-06-13 18:59:02 +0200

    urbanterror: removed because it's replaced by urbanterror-{client,server,data} Signed-off-by: Jelle van der Waa <jellevdwaa@gmail.com>

--- packages/urbanterror/urbanterror-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-if [ ! -e "$HOME/.urbanterror" ]; then mkdir $HOME/.urbanterror; fi
-cd /opt/urbanterror/
-exec ./ioUrTded +set fs_homepath $HOME/.urbanterror $*
--- packages/urbanterror/urtded.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /opt/urbanterror/
-./ioUrTded +set cl_curllib /usr/lib/libcurl.so
